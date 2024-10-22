22c0cf2adef93ebb3fcfe18b1dc97e94987f5a41
JunzheLiu@helium2 <junzliu@126.com>
2024-10-22 18:52:49 +0800

    fix the bug for channel index

--- channels.f
+++ channels.f
@@ -40,6 +40,8 @@
                     channel_index%ch_numbers = channel_index%ch_numbers + 1
                 end do
             end do
+            write(*,201) channel_index%ch_numbers
+201         FORMAT(' There are ', I4,' different channels ')
 
             allocate(channel_index%L(1:channel_index%ch_numbers))
             allocate(channel_index%S(1:channel_index%ch_numbers))
@@ -49,18 +51,17 @@
             ich = 1
             do il = lmin, lmax
                 do iJ = nint(2 * abs(il - sp)), nint(2 * (il + sp)), 2
+                    if(iJ .lt. 2*jmin) cycle
+                    if(iJ .gt. 2*jmax) cycle
                     channel_index%L(ich) = il
                     channel_index%S(ich) = sp
                     channel_index%J(ich) = iJ/2d0
-                    write(*,1000) ich, channel_index%L(ich), channel_index%S(ich), channel_index%J(ich)
                     write(11,1000) ich, channel_index%L(ich), channel_index%S(ich), channel_index%J(ich)
                     ich = ich + 1
                 end do
             end do
 
-            write(*,204) lmin,lmax
-            write(*,205) sp, (lmax+sp)
-204         FORMAT('       L Range:', I5,' <= L <= ',I5)
+            write(*,205) jmin,jmax
 205         FORMAT(' Total J Range:', f5.1,' <= J <= ',f5.1)
             write(*,*)
 
